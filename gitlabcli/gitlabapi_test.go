6303ad0041eec5c29e08e9ef133fa5dd1c191b86
Didier Fabert <didier.fabert@gmail.com>
2025-08-12 14:21:30 +0200

    Improve test #2

--- gitlabcli/gitlabapi_test.go
+++ gitlabcli/gitlabapi_test.go
@@ -1,7 +1,6 @@
 package gitlabcli
 
 import (
-	"net/http"
 	"testing"
 )
 
@@ -20,84 +19,5 @@ func TestGLApiInit(t *testing.T) {
 	}
 	if glapi.Verbose != verbose {
 		t.Errorf(`TestGLApiInit(verbose) = %t, want %t`, glapi.Verbose, verbose)
-	}
-}
-
-func TestGLApiCallAllProjects(t *testing.T) {
-	url := "http://localhost:8080"
-	verbose := false
-	path := "/api/v4/projects?page=2&per_page=2"
-	expected := `[{"id":3,"name":"Ludwig Van Beethoven","description":null,"path":"ludwig_van_beethoven","name_with_namespace":"CPP Language / Ludwig Van Beethoven","path_with_namespace":"cpp_language/ludwig_van_beethoven","ssh_url_to_repo":"git@localhost:cpp_language/ludwig_van_beethoven.git","http_url_to_repo":"http://localhost:8080/cpp_language/ludwig_van_beethoven.git","web_url":"http://localhost:8080/cpp_language/ludwig_van_beethoven","visibility":"public"},{"id":4,"name":"Johannes Brahms","description":null,"path":"johannes_brahms","name_with_namespace":"CPP Language / Johannes Brahms","path_with_namespace":"cpp_language/johannes_brahms","ssh_url_to_repo":"git@localhost:cpp_language/johannes_brahms.git","http_url_to_repo":"http://localhost:8080/cpp_language/johannes_brahms.git","web_url":"http://localhost:8080/cpp_language/johannes_brahms","visibility":"public"}]`
-
-	glapi := NewGLApi(url, "", verbose)
-	resp, nbPage, err := glapi.CallGitlabApi(path, http.MethodGet, nil)
-	if err != nil {
-		t.Errorf(`CallGitlabApi(err) = %s`, err)
-	}
-	if nbPage == 0 {
-		t.Errorf(`CallGitlabApi(nbPage) = %d`, nbPage)
-	}
-	if len(resp) == 0 {
-		t.Errorf(`CallGitlabApi(resp len) = %d`, len(resp))
-	}
-	if string(resp) != expected {
-		t.Errorf(`CallGitlabApi(resp) = %s`, resp)
-	}
-}
-
-func TestGLApiCallProject(t *testing.T) {
-	url := "http://localhost:8080"
-	verbose := false
-	path := "/api/v4/projects/3"
-	expected := `[{"id":3,"name":"Ludwig Van Beethoven","description":null,"path":"ludwig_van_beethoven","name_with_namespace":"CPP Language / Ludwig Van Beethoven","path_with_namespace":"cpp_language/ludwig_van_beethoven","ssh_url_to_repo":"git@localhost:cpp_language/ludwig_van_beethoven.git","http_url_to_repo":"http://localhost:8080/cpp_language/ludwig_van_beethoven.git","web_url":"http://localhost:8080/cpp_language/ludwig_van_beethoven","visibility":"public"}]`
-
-	glapi := NewGLApi(url, "", verbose)
-	resp, _, err := glapi.CallGitlabApi(path, http.MethodGet, nil)
-	if err != nil {
-		t.Errorf(`CallGitlabApi(err) = %s`, err)
-	}
-	if len(resp) == 0 {
-		t.Errorf(`CallGitlabApi(resp len) = %d`, len(resp))
-	}
-	if string(resp) != expected {
-		t.Errorf(`CallGitlabApi(resp) = %s`, resp)
-	}
-}
-
-func TestGLApiCallVariables(t *testing.T) {
-	url := "http://localhost:8080"
-	verbose := false
-	path := "/api/v4/projects/3/variables"
-	expected := `[{"key":"DEBUG_ENABLED","value":"1","description":null,"environment_scope":"*","raw":true,"hidden":false,"protected":false,"masked":false},{"key":"DEBUG_ENABLED","value":"1","description":null,"environment_scope":"staging","raw":true,"hidden":false,"protected":false,"masked":false},{"key":"DEBUG_ENABLED","value":"1","description":null,"environment_scope":"production","raw":true,"hidden":false,"protected":false,"masked":false}]`
-
-	glapi := NewGLApi(url, "", verbose)
-	resp, _, err := glapi.CallGitlabApi(path, http.MethodGet, nil)
-	if err != nil {
-		t.Errorf(`CallGitlabApi(err) = %s`, err)
-	}
-	if len(resp) == 0 {
-		t.Errorf(`CallGitlabApi(resp len) = %d`, len(resp))
-	}
-	if string(resp) != expected {
-		t.Errorf(`CallGitlabApi(resp) = %s`, resp)
-	}
-}
-
-func TestGLApiCallEnvironments(t *testing.T) {
-	url := "http://localhost:8080"
-	verbose := false
-	path := "/api/v4/projects/3/environments"
-	expected := `[{"id":5,"name":"Staging","state":"available","external_url":null,"description":null},{"id":6,"name":"Production","state":"available","external_url":null,"description":null}]`
-
-	glapi := NewGLApi(url, "", verbose)
-	resp, _, err := glapi.CallGitlabApi(path, http.MethodGet, nil)
-	if err != nil {
-		t.Errorf(`CallGitlabApi(err) = %s`, err)
-	}
-	if len(resp) == 0 {
-		t.Errorf(`CallGitlabApi(resp len) = %d`, len(resp))
-	}
-	if string(resp) != expected {
-		t.Errorf(`CallGitlabApi(resp) = %s`, resp)
 	}
 }
--- gitlabcli/gitlabcli_test.go
+++ gitlabcli/gitlabcli_test.go
@@ -1,1 +1,81 @@
 package gitlabcli
+
+import (
+	"testing"
+)
+
+func TestGLCliConfig(t *testing.T) {
+	url := "https://gitlab.com"
+	idfile := "project.id"
+	varsfile := "project.vars"
+	envsfile := "project.envs"
+	projectsfile := "projects.list"
+	debugfile := "debug.log"
+	tokenfile := "project.token"
+	remoteName := "origin"
+	debug := true
+	verbose := true
+	dryrun := true
+	export := true
+	exportProject := true
+	delete := true
+
+	config := GLCliConfig{}
+	config.GitlabUrl = url
+	config.IdFile = idfile
+	config.VarsFile = varsfile
+	config.EnvsFile = envsfile
+	config.ProjectsFile = projectsfile
+	config.DebugFile = debugfile
+	config.TokenFile = tokenfile
+	config.RemoteName = remoteName
+	config.DebugMode = debug
+	config.VerboseMode = verbose
+	config.DryrunMode = dryrun
+	config.ExportMode = export
+	config.ExportProjectMode = exportProject
+	config.DeleteMode = delete
+
+	if config.GitlabUrl != url {
+		t.Errorf(`TestGLCliConfig(GitlabUrl) = %s, want %s`, config.GitlabUrl, url)
+	}
+	if config.IdFile != idfile {
+		t.Errorf(`TestGLCliConfig(IdFile) = %s, want %s`, config.IdFile, idfile)
+	}
+	if config.VarsFile != varsfile {
+		t.Errorf(`TestGLCliConfig(VarsFile) = %s, want %s`, config.VarsFile, varsfile)
+	}
+	if config.EnvsFile != envsfile {
+		t.Errorf(`TestGLCliConfig(EnvsFile) = %s, want %s`, config.EnvsFile, envsfile)
+	}
+	if config.ProjectsFile != projectsfile {
+		t.Errorf(`TestGLCliConfig(ProjectsFile) = %s, want %s`, config.ProjectsFile, projectsfile)
+	}
+	if config.DebugFile != debugfile {
+		t.Errorf(`TestGLCliConfig(DebugFile) = %s, want %s`, config.DebugFile, debugfile)
+	}
+	if config.TokenFile != tokenfile {
+		t.Errorf(`TestGLCliConfig(TokenFile) = %s, want %s`, config.TokenFile, tokenfile)
+	}
+	if config.RemoteName != remoteName {
+		t.Errorf(`TestGLCliConfig(RemoteName) = %s, want %s`, config.RemoteName, remoteName)
+	}
+	if config.DebugMode != debug {
+		t.Errorf(`TestGLCliConfig(DebugMode) = %t, want %t`, config.DebugMode, debug)
+	}
+	if config.VerboseMode != verbose {
+		t.Errorf(`TestGLCliConfig(VerboseMode) = %t, want %t`, config.VerboseMode, verbose)
+	}
+	if config.DryrunMode != dryrun {
+		t.Errorf(`TestGLCliConfig(DryrunMode) = %t, want %t`, config.DryrunMode, dryrun)
+	}
+	if config.ExportMode != export {
+		t.Errorf(`TestGLCliConfig(ExportMode) = %t, want %t`, config.ExportMode, export)
+	}
+	if config.ExportProjectMode != exportProject {
+		t.Errorf(`TestGLCliConfig(ExportProjectMode) = %t, want %t`, config.ExportProjectMode, exportProject)
+	}
+	if config.DeleteMode != delete {
+		t.Errorf(`TestGLCliConfig(DeleteMode) = %t, want %t`, config.DeleteMode, delete)
+	}
+}
